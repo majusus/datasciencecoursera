81c88e6db0cf075c6c516490c021e6bfe3533168
majusus <sushamjob13@gmail.com>
2019-09-10 20:49:40 +0530

    week 2 and 3 materials added

---
+++ Mongo/intro-to-mongodb/mflix/mflix 2/env.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=mflix/mflix.py
+set FLASK_DEBUG=false
+set MFLIX_DB_URI="mongodb+srv://analytics:analytics-password@mflix-x6jfv.mongodb.net/test?retryWrites=true&w=majority" & REM Replace XXXX with your MongoDB Connection URI
